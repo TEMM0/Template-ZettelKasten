1ad55c25cd5b1ded981a1bd8cebd3680a347baea
TEMM0 <temir94@mail.ru>
2021-05-03 21:49:20 +0800

    no need to import class

--- gui_filemake.py
+++ gui_filemake.py
@@ -1,8 +1,35 @@
 import PySimpleGUI as sg
-from file_creator import Note
 import time
 import os
 
+
+class Note:
+  def __init__(self):
+    gtmtime=time.gmtime(time.time())
+    year = str(int(gtmtime.tm_year))
+    month=str(int(gtmtime.tm_mon))
+    day=str(int(gtmtime.tm_mday))
+    hour=str(int(gtmtime.tm_hour))
+    minutes=str(int(gtmtime.tm_min))
+    seconds=str(int(gtmtime.tm_sec))
+    self.code=year+month+day+hour+minutes+seconds;      
+  
+  def write_to_file(self, title):
+    self.title=title;
+    file = open(self.code+" - "+self.title+".md", "w")
+    file.write("---\n")
+    file.write("id: "+self.code+"\n")
+    file.write("title: "+self.title+"\n")
+    file.write("keywords: \n"+"#keyword1\n");
+    file.write("---\n")
+    file.write("Zettel Link: [[zettel_url]]\n")
+    file.write("Citation - Your idea\n")
+    file.write("Outside Link: "+ "[VK](https://vk.com/)\n")
+    file.close()
+    
+
+metafile = open("metadata", "w")
+metafile.close()
 
 metafile = open("metadata", "r")
 saved_vault = metafile.readline()
